3f4484a4cb559cfebf159acb472615e13af35d19
Erik Tjong Kim Sang <erikt@xs4all.nl>
2018-02-13 18:26:10 +0100

    added 10cv

--- fasttextRun.py
+++ fasttextRun.py
@@ -18,7 +18,24 @@ COMMAND = sys.argv[0]
 USAGE = "usage: "+COMMAND+" -T trainFile -t testFile [-l]"
 DIM = 300
 MINCOUNT = 5
+NBROFFOLDS = 10
+TMPFILENAME = COMMAND+"."+str(os.getpid())+"."+str(int(random.randrange(1000000)))
 showLabels = False
+
+def processOpts(argv):
+    global showLabels
+
+    argv.pop(0)
+    try: options = getopt.getopt(argv,"T:t:l",[])
+    except: sys.exit(USAGE)
+    trainFile = ""
+    testFile = ""
+    for option in options[0]:
+        if option[0] == "-T": trainFile = option[1]
+        elif option[0] == "-t": testFile = option[1]
+        elif option[0] == "-l": showLabels = True
+    if trainFile == "": sys.exit(USAGE)
+    return(trainFile,testFile)
 
 def readData(inFileName):
     text = []
@@ -34,42 +51,63 @@ def readData(inFileName):
     return({"text":text, "classes":classes})
 
 def runExperiment(trainFileName,testFileName):
-    tmpFileName = COMMAND+"."+str(os.getpid())+"."+str(int(random.randrange(1000000)))
+    global TMPFILENAME, DIM, MINCOUNT
+
     model = FastText()
-    model.supervised(input=trainFileName,output=tmpFileName,dim=DIM,minCount=MINCOUNT,verbose=0)
+    model.supervised(input=trainFileName,output=TMPFILENAME,dim=DIM,minCount=MINCOUNT,verbose=0)
     labels = model.predict_file(testFileName)
     data = readData(testFileName)
     correct = 0
     for i in range(0,len(labels)): 
         data["classes"][i] = re.sub("__label__","",data["classes"][i])
         if labels[i][0] == data["classes"][i]: correct += 1
-    correct *= 100/len(data["classes"])
-    os.unlink(tmpFileName+".bin")
-    os.unlink(tmpFileName+".vec")
+    os.unlink(TMPFILENAME+".bin")
+    os.unlink(TMPFILENAME+".vec")
     return({"correct":correct,"labels":labels})
 
-def processOpts(argv):
-    global showLabels
+def writeFile(fileName,text,labels):
+    with open(fileName,"w") as f:
+        for i in range(0,len(labels)):
+            f.write(labels[i])
+            for token in text[i]: f.write(" "+token)
+            f.write("\n")
+        f.close()
 
-    argv.pop(0)
-    try: options = getopt.getopt(argv,"T:t:l",[])
-    except: sys.exit(USAGE)
-    trainFile = ""
-    testFile = ""
-    for option in options[0]:
-        if option[0] == "-T": trainFile = option[1]
-        elif option[0] == "-t": testFile = option[1]
-        elif option[0] == "-l": showLabels = True
-    if trainFile == "" or testFile == "": sys.exit(USAGE)
-    return(trainFile,testFile)
+def printResults(correct,nbrOfLabels,prefix):
+    print("{1:s}Correct: {0:0.1f}%".format(100*correct/nbrOfLabels,prefix))
+
+
+def run10cv(trainFileName):
+    global TMPFILENAME, NBROFFOLDS
+
+    data = readData(trainFileName)
+    classes = data["classes"]
+    text = data["text"]
+    trainFileName = TMPFILENAME+".train"
+    testFileName = TMPFILENAME+".test"
+    totalCorrect = 0
+    labels = []
+    for i in range(0,NBROFFOLDS):
+        testStart = int(float(i)*float(len(text))/float(NBROFFOLDS))
+        testEnd = int(float(i+1)*float(len(text))/float(NBROFFOLDS))
+        writeFile(testFileName,text[testStart:testEnd],classes[testStart:testEnd])
+        writeFile(trainFileName,text[:testStart]+text[testEnd:],classes[:testStart]+classes[testEnd:])
+        results = runExperiment(trainFileName,testFileName)
+        totalCorrect += results["correct"]
+        labels.extend(results["labels"])
+        if not showLabels:
+            printResults(results["correct"],len(results["labels"]),"Fold: {0:2d}; ".format(i+1))
+    os.unlink(trainFileName)
+    os.unlink(testFileName)
+    return({"correct":totalCorrect,"labels":labels})
 
 def main(argv):
     global showLabels
 
     trainFileName, testFileName = processOpts(argv)
-    results = runExperiment(trainFileName,testFileName)
-    if not showLabels: 
-        print("correct: {0:0.1f}%".format(results["correct"]))
+    if testFileName != "": results = runExperiment(trainFileName,testFileName)
+    else: results = run10cv(trainFileName)
+    if not showLabels: printResults(results["correct"],len(results["labels"]),"")
     else:
         for i in range(0,len(results["labels"])):
             print(results["labels"][i][0])
