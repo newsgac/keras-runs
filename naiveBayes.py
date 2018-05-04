db4744562f719e6eed1f892bf35b4a35dc2e11dc
Erik Tjong Kim Sang <erikt@xs4all.nl>
2018-05-04 18:27:25 +0200

    added tf-idf

--- naiveBayes.py
+++ naiveBayes.py
@@ -5,16 +5,21 @@
 
 import numpy as np
 import sys
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB, GaussianNB, MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import cross_val_score
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.model_selection import cross_val_predict
+from sklearn import metrics
 
 COMMAND = sys.argv.pop(0)
 CVSIZE = 10
 USAGE = "usage: "+COMMAND+" data-file"
 ANALYZER = "word"
+MINDF = 0.01
+MAXDF = 0.5
 NGRAMMIN = 1
-NGRAMMAX = 5
+NGRAMMAX = 1
+NBMODEL = BernoulliNB()
 
 def readData(dataFileName):
     data = []
@@ -36,12 +41,17 @@ def tokenizer(text):
     return(text.split())
 
 def makeNumericText(texts):
-    count_vect = CountVectorizer(
-                 ngram_range=(NGRAMMIN,NGRAMMAX),
-                 analyzer=ANALYZER,
-                 tokenizer=tokenizer)
-    text_counts = count_vect.fit_transform(texts)
-    return(text_counts)
+    countsModel = CountVectorizer(
+                  analyzer=ANALYZER,
+                  max_df=MAXDF,
+                  min_df=MINDF,
+                  ngram_range=(NGRAMMIN,NGRAMMAX),
+                  tokenizer=tokenizer)
+    textCounts = countsModel.fit_transform(texts)
+    tfidfModel = TfidfTransformer()
+    textTfidf = tfidfModel.fit_transform(textCounts)
+    print(textTfidf.shape)
+    return(textTfidf,countsModel,tfidfModel)
 
 def makeNumericList(thisList):
     cellNames = {}
@@ -55,40 +65,34 @@ def makeNumericList(thisList):
     return(thisListN,cellNames)
 
 def naiveBayesTrain(examples,labels):
-    return(MultinomialNB().fit(examples,labels))
+    return(NBMODEL.fit(examples,labels))
 
-def naiveBayesTest(model,examples):
-    return(model.predict(examples))
+def naiveBayesTest(model,examples,labels):
+    predictedLabels = model.predict(examples)
+    return(metrics.accuracy_score(labels,predictedLabels),predictedLabels)
 
 def naiveBayes10cv(examples,labels,cvSize):
-    return(cross_val_score(MultinomialNB(),examples,labels,cv=cvSize))
+    predictedLabels = cross_val_predict(NBMODEL,examples,labels,cv=cvSize)
+    return(metrics.accuracy_score(labels,predictedLabels),predictedLabels)
 
-def compare(predictions,gold):
-    correctCount = 0
-    if len(predictions) != len(gold):
-        sys.exit(COMMAND+": array length error in compare()\n")
-    for i in range(0,len(predictions)):
-        if predictions[i] == gold[i]: correctCount += 1
-    return(correctCount)
-
-def showResult(labels,correctCount):
+def showResult(labels,accuracy):
     nbrOfLabels = len(labels)
-    percentage = int(round(100*correctCount/len(labels)))
+    correctCount = int(round(nbrOfLabels*accuracy))
+    percentage = int(round(accuracy*100))
     print("correct:",str(correctCount),"of",str(nbrOfLabels),"("+str(percentage)+"%)")
 
 def main(argv):
     try: dataFileName = sys.argv.pop(0)
     except: sys.exit(USAGE)
     data,labels = readData(dataFileName)
-    dataN = makeNumericText(data)
+    dataN,countsModel,tfidfModel = makeNumericText(data)
     labelsN,labelNames = makeNumericList(labels)
     model = naiveBayesTrain(dataN,labelsN)
-    predictions = naiveBayesTest(model,dataN)
-    correctCount = compare(predictions,labelsN)
-    showResult(labels,correctCount)
-    scores = naiveBayes10cv(dataN,labelsN,CVSIZE)
-    correctCount = int(round(np.mean(scores)*len(labels)))
-    showResult(labels,correctCount)
+    accuracy,predictedLabels = naiveBayesTest(model,dataN,labelsN)
+    showResult(labels,accuracy)
+    accuracy,predictedLabels = naiveBayes10cv(dataN,labelsN,CVSIZE)
+    showResult(labels,accuracy)
+    print(metrics.confusion_matrix(labelsN,predictedLabels))
     sys.exit(0)
 
 if __name__ == "__main__":
